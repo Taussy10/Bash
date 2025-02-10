84969251e4ce40c8336e02a54fd2bb592d458bbc
Tausif <tausif00100@gmail.com>
2025-02-11 00:45:14 +0530

    learned bash command for writing , reading etc

---
+++ 01-Intro-To-Terminal/index.sh
@@ -0,0 +1,15 @@
+name=Tausif
+
+echo $num
+
+
+# For making 10 files by bash command
+# for i in {1..10}; do touch "app$i.js"; done
+
+# For removing 10 files by bash command
+for i in {1..10}; do rm "app$i.js"; done
+
+# To run the file in bash terminal
+# ./index.sh 
+
+# Writing this using nano command in index.sh
--- 01-Intro-To-Terminal/test.sh
+++
@@ -1,6 +0,0 @@
-num=Tausif
-
-echo $num
-
-# To run the file in bash terminal
-# ./test.sh 
